90983e869b71461d769dccfe59555058bbfc6e83
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2021-05-17 13:36:24 +0200

    Reproduce the issue + workaround

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace WpfApp
+﻿using System.Collections.Generic;
+using System.Windows.Documents;
+
+namespace WpfApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +11,29 @@
         public MainWindow()
         {
             InitializeComponent();
+            var items = new List<Person>
+            {
+                new Person("osama", "albanna", "0223355556625", 25),
+                new Person("osama - osama - osama ", "albanna - albanna - albanna", "0223355556625", 25),
+            };
+
+            this.FirstItemsCollection.ItemsSource = items;
+            this.SecondItemsCollection.ItemsSource = items;
+        }
+        
+        public class Person
+        {
+            public Person(string firstname, string lastname, string phone, int age)
+            {
+                Firstname = firstname;
+                Lastname = lastname;
+                Phone = phone;
+                Age = age;
+            }
+            public string Firstname { get; set; }
+            public string Lastname { get; set; }
+            public string Phone { get; set; }
+            public int Age { get; set; }
         }
     }
 }
